166e3023ffc533561653971a917b324564bc2187
tahaontech <tahatechbusiness@gmail.com>
2022-08-03 09:00:46 +0430

    first step done

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,47 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script context="module">
+  export const load = async ({ fetch }) => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const posts = await res.json();
+    return {
+      props: {
+        posts,
+      },
+    };
+  };
+</script>
+
+<script>
+  export let posts;
+</script>
+
+<h1>Posts</h1>
+
+<div class="posts">
+  {#each posts as item}
+    <div class="post">
+      <h2>{item.title}</h2>
+      <p>{item.body}</p>
+      <p class="links"><a sveltekit:prefetch href={`/blogs/${item.id}`} > Read More </a></p>
+    </div>
+  {/each}
+</div>
+
+<style>
+  .posts {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-gap: 20px;
+  }
+
+  .post {
+    padding: 20px;
+    border: 1px solid #ddd;
+    box-shadow: 0 0 10px #eee;
+  }
+  h2 {
+    margin: 0;
+  }
+  .links {
+    text-align: right;
+  }
+</style>
